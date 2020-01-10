af5fc1bbb91bdd71b71d2725b7959d74202a3d2b
le.yan <le.yan@dmall.com>
2020-01-10 13:44:16 +0800

    Initial commit

---
+++ design-pattern-factory-method/src/main/java/com/yanle/dp/DesignPatternFactoryMethodApplication.java
@@ -0,0 +1,13 @@
+package com.yanle.dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternFactoryMethodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternFactoryMethodApplication.class, args);
+    }
+
+}
---
+++ design-pattern-factory-method/src/test/java/com/yanle/dp/DesignPatternFactoryMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanle.dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternFactoryMethodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
